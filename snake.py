edef244eec9e026f4e95ebc21433a251130299e1
Piper Virtue <grantvirtue@outlook.com>
2021-04-21 10:00:23 -0400

    Updates to collision control

--- snake.py
+++ snake.py
@@ -6,18 +6,27 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
-dis=pygame.display.set_mode((800,600))
+dis_width = 800
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake game by Gwen')
 
 game_over=False
 
-x1 = 300
-y1 = 300
+x1 = dis_width/2
+y1 = dis_height/2
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
+snake_speed=30
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg, color):
+	mesg = font_style.render(msg, True, color)
+	dis.blit(mesg, [dis_width/2, dis_height/2])
 
 while not game_over:
 	for event in pygame.event.get():
@@ -37,6 +46,9 @@ while not game_over:
 				y1_change = 10
 				x1_change = 0
 
+	if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+		game_over = True
+
 	x1 += x1_change
 	y1 += y1_change
 	dis.fill(white)
@@ -44,7 +56,11 @@ while not game_over:
 
 	pygame.display.update()
 
-	clock.tick(30)
+	clock.tick(snake_speed)
+
+message("You lost", red)
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()
 quit()
